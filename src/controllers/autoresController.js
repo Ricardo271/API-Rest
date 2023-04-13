56f0533038a6d25b5a9b4d37a65189b5c27d95da
Ricardo271 <ric.zanluchi@gmail.com>
2023-04-13 17:58:12 -0300

    Inicio do tratamento de erros

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import autores from "../models/Autor.js";
 
 class AutorController {
@@ -7,7 +8,7 @@ class AutorController {
             const autores_resultado = await autores.find();
             res.status(200).json(autores_resultado);
         }catch (err) {
-            res.status(500).json({message: "Erro interno do servidor"});
+            res.status(500).json({message: "Erro interno do servidor."});
         }
 
     };
@@ -16,9 +17,21 @@ class AutorController {
         try {
             const id = req.params.id;
             const autor_resultado = await autores.findById(id);
-            res.status(200).json(autor_resultado);
+
+            if (autor_resultado !== null) {
+                res.status(200).json(autor_resultado);
+            } else {
+                res.status(404).json({message: "Id do autor não localizado."});
+            }
+
         } catch (err) {
-            res.status(400).json({message: `${err.message} - Id do autor não localizado.`});
+
+            if (err instanceof mongoose.Error.CastError) {
+                res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos."});
+            } else {
+                res.status(500).send({message: "Erro interno de servidor."});
+            }
+
         }
     };
 

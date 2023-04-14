ab55b0b83771a428e00f6bee5b169b2a2926129f
Ricardo271 <ric.zanluchi@gmail.com>
2023-04-14 12:22:07 -0300

    Validador global para Strings vazias implementado

--- src/Erros/NaoEncontrado.js
+++ src/Erros/NaoEncontrado.js
@@ -2,7 +2,7 @@ import ErroBase from "./ErroBase.js";
 
 class NaoEncontrado extends ErroBase {
     constructor(mensagem = "Página não encontrada") {
-        super(mensagem, 404);
+        super(mensagelivrosm, 404);
     }
 }
 

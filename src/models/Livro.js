2274cea540d078f0c35a0f8e7d43a2d9f15e62b5
Ricardo271 <ric.zanluchi@gmail.com>
2023-04-14 11:49:08 -0300

    Validações do mongoose implementadas

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,10 +3,28 @@ import mongoose from "mongoose";
 const livro_schema = new mongoose.Schema(
     {
         id: { type: String },
-        titulo: { type: String, required: [true, "O título é obrigatório"] },
-        autor: { type: mongoose.Schema.Types.ObjectId, ref: "autores", required: [true, "O autor é obrigatório"] },
-        editora: { type: String, required: [true, "A editora é obrigatória"] },
-        numeroPaginas: { type: Number }
+        titulo: {
+            type: String,
+            required: [true, "O título é obrigatório"]
+        },
+        autor: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "autores",
+            required: [true, "O autor é obrigatório"]
+        },
+        editora: {
+            type: String,
+            required: [true, "A editora é obrigatória"],
+            enum: {
+                values: ["Casa do Código", "Alura"],
+                message: "A editora {VALUE} não é um valor permitido"
+            }
+        },
+        numeroPaginas: {
+            type: Number,
+            min: [1, "O número de páginas deve estar entre 1 e 20000. Valor fornecido: {VALUE}"],
+            max: [20000, "O número de páginas deve estar entre 1 e 20000. Valor fornecido: {VALUE}"]
+        }
     }
 );
 

6cac36528c869f0aeadb20f9a6f6235fb935b74e
Ricardo271 <ric.zanluchi@gmail.com>
2023-04-18 17:37:30 -0300

    Erro na variável da mensagem corrigido

--- src/Erros/NaoEncontrado.js
+++ src/Erros/NaoEncontrado.js
@@ -2,7 +2,7 @@ import ErroBase from "./ErroBase.js";
 
 class NaoEncontrado extends ErroBase {
     constructor(mensagem = "Página não encontrada") {
-        super(mensagelivrosm, 404);
+        super(mensagem, 404);
     }
 }
 
